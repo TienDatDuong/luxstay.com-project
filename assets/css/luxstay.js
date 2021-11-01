09609c61555cb4b57241ebb3970ad01fff0600f6
duongtiendat283251.github.io <89386979+Duongtiendat93@users.noreply.github.com>
2021-11-01 13:30:37 +0700

    luxstay project

---
+++ assets/css/luxstay.js
@@ -0,0 +1,59 @@
+function incrementValueS() {
+    var value = document.querySelector('#number').value;
+    isNaN(value) ? 0 : value;
+    // value--;
+    if(value<0){
+        value === 0
+    }else{
+        if(value>0) {
+            value--
+        }
+    }
+    document.querySelector('#number').value = value;
+}
+function incrementValue() {
+    var value = document.querySelector('#number').value;
+    isNaN(value) ? 0 : value;
+    value ++
+    document.querySelector('#number').value = value;
+}
+//trẻ em
+function incrementValueS2() {
+    var value = document.querySelector('#number2').value;
+    isNaN(value) ? 0 : value;
+    // value--;
+    if(value<0){
+        value === 0
+    }else{
+        if(value>0) {
+            value--
+        }
+    }
+    document.querySelector('#number2').value = value;
+}
+function incrementValue2() {
+    var value = document.querySelector('#number2').value;
+    isNaN(value) ? 0 : value;
+    value ++
+    document.querySelector('#number2').value = value;
+}
+//trẻ sơ sinh
+function incrementValueSlast() {
+    var value = document.querySelector('#numberlast').value;
+    isNaN(value) ? 0 : value;
+    // value--;
+    if(value<0){
+        value === 0
+    }else{
+        if(value>0) {
+            value--
+        }
+    }
+    document.querySelector('#numberlast').value = value;
+}
+function incrementValuelast() {
+    var value = document.querySelector('#numberlast').value;
+    isNaN(value) ? 0 : value;
+    value ++
+    document.querySelector('#numberlast').value = value;
+}
